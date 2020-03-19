fb70b81c710b80bc2c7bed216fa8eee55e4323b4
tangellaz <tangellaz@gmail.com>
2020-03-19 05:18:14 -0700

    uploading redux files

---
+++ build/precache-manifest.849b58b25b8b21c8f4b64c7c7dbd3e1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f422a3e65de3cd8043e18aece14546b",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "d8977b7e97eaf4686f1b",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "c0c066aa3138153148a4",
+    "url": "/robofriends/static/css/main.d40f1321.chunk.css"
+  },
+  {
+    "revision": "d8977b7e97eaf4686f1b",
+    "url": "/robofriends/static/js/2.ff70bd64.chunk.js"
+  },
+  {
+    "revision": "c0c066aa3138153148a4",
+    "url": "/robofriends/static/js/main.9ac89506.chunk.js"
+  },
+  {
+    "revision": "6af7fd70da35e0253709",
+    "url": "/robofriends/static/js/runtime-main.8790a40a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
